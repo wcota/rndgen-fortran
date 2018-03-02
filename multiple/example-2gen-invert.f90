24ca003c3f1a693e7fac64e8384d1e170736ea4f
Wesley Cota <wlcota@gmail.com>
2018-03-02 11:00:46 -0300

    Update and rename exemplo-2geradores-invert.f90 to example-2gen-invert.f90

--- multiple/example-2gen-invert.f90
+++ multiple/example-2gen-invert.f90
@@ -2,40 +2,42 @@ program teste
 use mod_rndgen
 implicit none
 
-    ! Como usar:
-    ! ./a.out semente1 semente2
+    ! How to use:
+    ! ./a.out seed1 seed2
 
-    type(rndgen) :: geradores(2)    ! lista de geradores
-    integer :: aux                  ! auxiliar de leitura
-    character*64 :: caux            ! auxiliar de leitura
+    type(rndgen) :: geradores(2)    ! rnd list
+    integer :: aux                  ! aux
+    character*64 :: caux            ! aux
     integer :: i
 
     call getarg(1,caux)
     read(caux,*) aux
-    call geradores(1)%init(aux)
+    call geradores(1)%init(aux) ! use the first seed
     
     call getarg(2,caux)
     read(caux,*) aux
-    call geradores(2)%init(aux)
+    call geradores(2)%init(aux) ! use the second seed
     
     
-    write(*,*) "Escrevendo um de cada vez..."
-    print*, "Semente 1:", geradores(1)%o_iseed
+    write(*,*) "Writing 10 numbers..."
+    print*, "seed1:", geradores(1)%o_iseed
     do i = 1,10
         print*, geradores(1)%rnd()
     enddo
     
     write(*,*) ""
-    print*, "Semente 2:", geradores(2)%o_iseed
+    print*, "seed2:", geradores(2)%o_iseed
     do i = 1,10
         print*, geradores(2)%rnd()
     enddo
     
     write(*,*) ""
-    write(*,*) "Escrevendo ambos juntos após inverter as sementes. Deve dar a mesma sequência individualmente!"
+    write(*,*) "Inverting the seeds..."
     aux = geradores(1)%o_iseed
     call geradores(1)%init(geradores(2)%o_iseed)
     call geradores(2)%init(aux)
+    print*, "seed1:", geradores(1)%o_iseed
+    print*, "seed2:", geradores(2)%o_iseed
     do i = 1,10
         print*, geradores(1)%rnd(), geradores(2)%rnd()
     enddo
