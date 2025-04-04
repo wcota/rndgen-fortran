c277fb2c2b512b34be64694e9c371fddf447496a
Wesley Cota <wlcota@gmail.com>
2025-04-04 20:57:47 -0300

    update docs

--- docs/src/rndgen.f90
+++ docs/src/rndgen.f90
@@ -26,22 +26,20 @@
 ! Author    : Wesley Cota
 ! Email     : wesley@wcota.me
 ! Homepage  : http://wcota.me
-! Date      : 25 Feb 2024
-! Version   : 0.3.1
+! Date      : 04 Apr 2025
+! Version   : 1.0.0
 !-----------------------------------------------------------------------------
 
 module rndgen_mod
+   use rndgen_kinds_mod
    implicit none
    private
 
-   integer, parameter :: dp = selected_real_kind(15) ! 8-byte reals
-   integer, parameter :: i8 = selected_int_kind(8) ! 4-byte integers
-   integer, parameter :: i16 = selected_int_kind(16) ! 8-byte integers
    real(kind=dp), parameter :: am = 4.656612873077392578d-10 ! multiplier 1/2^31
 
    !> Random seeds object
    type :: rndSeed
-      integer(kind=i8), private :: mseed(4)
+      integer(kind=i4), private :: mseed(4)
    contains
       procedure :: saveToFile => saveToFile_rndSeed
       procedure :: readFromFile => readFromFile_rndSeed
@@ -49,14 +47,20 @@ module rndgen_mod
 
    !> Random number generator object with its procedures
    type :: rndgen
-      integer :: o_iseed ! seed used to generate the four new seeds
+      integer(kind=i4) :: o_iseed ! seed used to generate the four new seeds
       type(rndSeed) :: seed
 
    contains
 
-      procedure :: rnd => rnd_rndgen ! generates a random number in the range [0, 1)
-      procedure :: int => int_rndgen ! generates a random integer number in the range [i1, i2]
-      procedure :: real => real_rndgen ! generates a random real number in the range [r1, r2)
+      procedure :: rnd => rnd_rndgen_dp  ! generates a random number in the range [0, 1)
+      procedure :: int => int_rndgen_i4 ! generates a random integer number in the range [i1, i2]
+
+      ! aliases:
+      procedure :: int_i4 => int_rndgen_i4
+      procedure :: int_i8 => int_rndgen_i8
+
+      procedure :: real => real_rndgen_dp ! generates a random real number in the range [r1, r2)
+      procedure :: real_sp => real_rndgen_sp ! generates a random real number in the range [r1, r2)
 
       procedure :: init => init_rndgen
       procedure :: reset => reset_rndgen
@@ -64,13 +68,20 @@ module rndgen_mod
       procedure :: save_seed => save_seed_rndgen
       procedure :: read_seed => read_seed_rndgen
 
+      generic :: rnd_array => rnd_array_rnd, rnd_array_real, rnd_array_int_i4
+      procedure :: rnd_array_i4 => rnd_array_int_i4
+      procedure :: rnd_array_i8 => rnd_array_int_i8
+      procedure, private :: rnd_array_rnd
+      procedure, private :: rnd_array_real
+      procedure, private :: rnd_array_int_i4
+
    end type
 
    public :: rndgen, rndSeed
 
 contains
    !> Generates a random number in the range [0, 1)
-   function rnd_rndgen(this) result(rnd_number) ! KISS
+   function rnd_rndgen_dp(this) result(rnd_number) ! KISS
       ! Adapted from <http://web.mst.edu/~vojtat/class_5403/kiss05/rkiss05.f90> by Thomas Vojta
 
       class(rndgen) :: this
@@ -88,22 +99,40 @@ contains
       rnd_number = kiss*am ! returns in range [0, 1)
    end function
 
-   !> Generates a random integer number in the range [i1, i2]
-   function int_rndgen(this, i1, i2) result(rnd_number)
+   !> Generates a random integer number in the range [i1, i2], int64
+   function int_rndgen_i8(this, i1, i2) result(rnd_number)
       class(rndgen) :: this
-      integer(kind=i16), intent(in) :: i1, i2
-      integer(kind=i16) :: rnd_number
+      integer(kind=i8), intent(in) :: i1, i2
+      integer(kind=i8) :: rnd_number
 
       rnd_number = min(int(this%rnd()*(i2 + 1 - i1)) + i1, i2) ! returns in range [i1, i2]
    end function
 
-   !> Generates a random real number in the range [r1, r2)
-   function real_rndgen(this, r1, r2) result(rnd_number)
+   !> Generates a random integer number in the range [i1, i2], int32
+   function int_rndgen_i4(this, i1, i2) result(rnd_number)
+      class(rndgen) :: this
+      integer(kind=i4), intent(in) :: i1, i2
+      integer(kind=i4) :: rnd_number
+
+      rnd_number = min(int(this%rnd()*(i2 + 1 - i1)) + i1, i2) ! returns in range [i1, i2]
+   end function
+
+   !> Generates a random real number in the range [r1, r2), double
+   function real_rndgen_dp(this, r1, r2) result(rnd_number)
       class(rndgen) :: this
       real(kind=dp), intent(in) :: r1, r2
       real(kind=dp) :: rnd_number
 
       rnd_number = r1 + (r2 - r1)*this%rnd() ! returns in range [r1, r2)
+   end function
+
+   !> Generates a random real number in the range [r1, r2), single
+   function real_rndgen_sp(this, r1, r2) result(rnd_number)
+      class(rndgen) :: this
+      real(kind=sp), intent(in) :: r1, r2
+      real(kind=sp) :: rnd_number
+
+      rnd_number = real(this%real(real(r1, kind=dp), real(r2, kind=dp)), kind=sp) ! returns in range [r1, r2)
    end function
 
    !> Initializes the random number generator
@@ -112,15 +141,15 @@ contains
 
       class(rndgen) :: this
 
-      integer(kind=i8) :: idum, ia, im, iq, ir, iseed
-      integer(kind=i8) :: k, c1
+      integer(kind=i4) :: idum, ia, im, iq, ir, iseed, iseed_var
+      integer(kind=i4) :: k, c1
       real(kind=dp) :: rdum
 
       parameter(ia=16807, im=2147483647, iq=127773, ir=2836)
 
-      iseed = abs(iseed) ! must be positive!
+      iseed_var = abs(iseed) ! must be positive!
 
-      this%o_iseed = iseed
+      this%o_iseed = iseed_var
 
       !!! Test integer representation !!!
       c1 = -8
@@ -128,7 +157,7 @@ contains
       !     print *,c1
       if (c1 /= 536870911) stop 'Nonstandard integer representation. Stopped.'
 
-      idum = iseed
+      idum = iseed_var
       idum = abs(1099087573*idum)               ! 32-bit LCG to shuffle seeds
       if (idum == 0) idum = 1
       if (idum >= IM) idum = IM - 1
@@ -220,5 +249,50 @@ contains
       read (und, *) (this%mseed(i), i=1, 4)
 
    end subroutine
+
+   !> Generates a random array of real numbers in the range [0, 1)
+   function rnd_array_rnd(gen, n) result(arr)
+      class(rndgen), intent(in) :: gen
+      integer(kind=i4), intent(in) :: n
+      integer(kind=i4) :: i
+      real(kind=dp), allocatable :: arr(:)
+
+      arr = [(gen%rnd(), i = 1, n)]
+
+  end function rnd_array_rnd
+
+  !> Generates a random array of real numbers in the range [r1, r2)
+  function rnd_array_real(gen, n, r1, r2) result(arr)
+     class(rndgen), intent(in) :: gen
+     integer(kind=i4), intent(in) :: n
+     real(kind=dp), intent(in) :: r1, r2
+     integer(kind=i4) :: i
+     real(kind=dp), allocatable :: arr(:)
+
+     arr = [(gen%real(r1,r2), i = 1, n)]
+
+ end function rnd_array_real
+
+ function rnd_array_int_i4(gen, n, i1, i2) result(arr)
+     class(rndgen), intent(in) :: gen
+     integer(kind=i4), intent(in) :: n
+     integer(kind=i4), intent(in) :: i1, i2
+     integer(kind=i4) :: i
+     integer(kind=i4), allocatable :: arr(:)
+
+     arr = [(gen%int_i4(i1, i2), i = 1, n)]
+
+ end function rnd_array_int_i4
+
+ function rnd_array_int_i8(gen, n, i1, i2) result(arr)
+     class(rndgen), intent(in) :: gen
+     integer(kind=i4), intent(in) :: n
+     integer(kind=i8), intent(in) :: i1, i2
+     integer(kind=i4) :: i
+     integer(kind=i8), allocatable :: arr(:)
+
+     arr = [(gen%int_i8(i1, i2), i = 1, n)]
+
+ end function rnd_array_int_i8
 
 end module
