6d72b56f6b71e965272cb60570f06f1f65a58aca
Wesley Cota <wlcota@gmail.com>
2026-08-23 19:00:07 -0300

    format output files and update example programs for consistency

--- example/simple.f90
+++ example/simple.f90
@@ -1,50 +1,53 @@
 program example
-   use iso_fortran_env, only : i4 => int32, i8 => int64, sp => real32, dp => real64
-   use rndgen_mod
-   implicit none
+    use iso_fortran_env, only : i4 => int32, i8 => int64, sp => real32, dp => real64
+    use rndgen_mod, only: rndgen_base_t, rndgen_kiss_t
+    implicit none
 
-   integer(kind=i4) :: i
-   integer(kind=i4) :: seed = 294727492
+    integer(kind=i4) :: i
+    integer(kind=i4) :: seed = 294727492
 
-   ! Declare the generator
-   type(rndgen) :: generator
+    ! Declare the generator
+    class(rndgen_base_t), allocatable :: generator
 
-   ! Initialize it with the seed
-   call generator%init(seed)
+    ! Allocate the generator
+    allocate(rndgen_kiss_t :: generator)
 
-   write (*, *) "10 random U(0,1):"
-   do i = 1, 10
-      write (*, *) generator%rnd()
-   end do
+    ! Initialize it with the seed
+    call generator%init(seed)
 
-   write (*, *) "10 random integers between 5 and 2587:"
-   do i = 1, 10
-      write (*, *) generator%int(5, 2587)
-   end do
+    write (*, *) "10 random U(0,1):"
+    do i = 1, 10
+        write (*, *) generator%rnd()
+    end do
 
-   write (*, *) "10 random real between -5.2 and 100.9:"
-   do i = 1, 10
-      write (*, *) generator%real(-5.2_dp, 100.9_dp)
-   end do
+    write (*, *) "10 random integers between 5 and 2587:"
+    do i = 1, 10
+        write (*, *) generator%int(5, 2587)
+    end do
 
-   write (*, *) ""
-   write (*, *) ""
-   write (*, *) "Reset the generator and repeat"
-   call generator%reset()
+    write (*, *) "10 random real between -5.2 and 100.9:"
+    do i = 1, 10
+        write (*, *) generator%real(-5.2_dp, 100.9_dp)
+    end do
 
-   write (*, *) "10 random U(0,1):"
-   do i = 1, 10
-      write (*, *) generator%rnd()
-   end do
+    write (*, *) ""
+    write (*, *) ""
+    write (*, *) "Reset the generator and repeat"
+    call generator%reset()
 
-   write (*, *) "10 random integers between 5 and 2587:"
-   do i = 1, 10
-    write (*, *) generator%int(5, 2587)
-   end do
+    write (*, *) "10 random U(0,1):"
+    do i = 1, 10
+        write (*, *) generator%rnd()
+    end do
 
-   write (*, *) "10 random real between -5.2 and 100.9:"
-   do i = 1, 10
-      write (*, *) generator%real(-5.2_dp, 100.9_dp)
-   end do
+    write (*, *) "10 random integers between 5 and 2587:"
+    do i = 1, 10
+        write (*, *) generator%int(5, 2587)
+    end do
+
+    write (*, *) "10 random real between -5.2 and 100.9:"
+    do i = 1, 10
+        write (*, *) generator%real(-5.2_dp, 100.9_dp)
+    end do
 
 end program
