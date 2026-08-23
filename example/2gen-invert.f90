07052c74f70a474139c182a8a81f57ee70ae29d5
Wesley Cota <wlcota@gmail.com>
2026-08-23 19:17:19 -0300

    update example program to use rndgen_kiss_t and improve output clarity

--- example/2gen-invert.f90
+++ example/2gen-invert.f90
@@ -1,48 +1,48 @@
 program example
     use iso_fortran_env, only : i4 => int32, i8 => int64, sp => real32, dp => real64
-    use rndgen_mod
+    use rndgen_mod, only: rndgen_kiss_t
     implicit none
 
     ! How to use:
     ! ./a.out seed1 seed2
 
-    type(rndgen) :: generators(2)   ! rnd list
+    type(rndgen_kiss_t) :: generators(2)   ! rnd list
     integer(kind=i4) :: aux                  ! aux
     character(len=64) :: caux       ! aux
     integer(kind=i4) :: i
-    
+
     if (iargc() /= 2) stop 'give two arguments: seed1 seed2'
 
     call getarg(1,caux)
     read(caux,*) aux
     call generators(1)%init(aux) ! use the first seed
-    
+
     call getarg(2,caux)
     read(caux,*) aux
     call generators(2)%init(aux) ! use the second seed
-    
-    
+
+
     write(*,*) "Writing 10 numbers..."
-    print*, "seed1:", generators(1)%o_iseed
+    print*, "seed1:", generators(1)%oseed
     do i = 1,10
         print*, generators(1)%rnd()
     enddo
-    
+
     write(*,*) ""
-    print*, "seed2:", generators(2)%o_iseed
+    print*, "seed2:", generators(2)%oseed
     do i = 1,10
         print*, generators(2)%rnd()
     enddo
-    
+
     write(*,*) ""
     write(*,*) "Inverting the seeds and generating at the same time..."
-    aux = generators(1)%o_iseed
-    call generators(1)%init(generators(2)%o_iseed)
+    aux = generators(1)%oseed
+    call generators(1)%init(generators(2)%oseed)
     call generators(2)%init(aux)
-    print*, "seed1:", generators(1)%o_iseed
-    print*, "seed2:", generators(2)%o_iseed
+    print*, "seed1:", generators(1)%oseed
+    print*, "seed2:", generators(2)%oseed
     do i = 1,10
         print*, generators(1)%rnd(), generators(2)%rnd()
     enddo
-    
+
 end program
