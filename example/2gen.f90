4e60b657ffc0e2f814ed04177d003d6e5f925b34
Wesley Cota <wlcota@gmail.com>
2026-08-23 21:07:38 -0300

    enhance example to support multiple random number generators and improve output formatting

--- example/2gen.f90
+++ example/2gen.f90
@@ -1,45 +1,71 @@
-program example
+program example_multiple_streams
     use iso_fortran_env, only : i4 => int32, i8 => int64, sp => real32, dp => real64
-    use rndgen_mod, only: rndgen_kiss_t
+    use rndgen_mod, only: rndgen_base_t, rndgen_kiss_t, rndgen_xoshiro256_t
     implicit none
 
     ! How to use:
     ! ./a.out seed1 seed2
 
-    type(rndgen_kiss_t) :: generators(2)   ! rnd list
-    integer(kind=i4) :: aux                  ! aux
-    character(len=64) :: caux       ! aux
-    integer(kind=i4) :: i
+    class(rndgen_base_t), allocatable :: generators(:)
+    integer(kind=i4) :: seed1, seed2
+    character(len=64) :: caux
 
-    if (iargc() /= 2) stop 'give two arguments: seed1 seed2'
+    if (command_argument_count() /= 2) stop 'give two arguments: seed1 seed2'
 
-    call getarg(1,caux)
-    read(caux,*) aux
-    call generators(1)%init(aux)
+    call get_command_argument(1, caux)
+    read(caux, *) seed1
 
-    call getarg(2,caux)
-    read(caux,*) aux
-    call generators(2)%init(aux)
+    call get_command_argument(2, caux)
+    read(caux, *) seed2
 
+    ! Test the two streams with KISS and Xoshiro256**
+    allocate(rndgen_kiss_t :: generators(2))
+    call run_two_streams(generators, "KISS", seed1, seed2)
+    deallocate(generators)
 
-    write(*,*) "Writing 10 numbers..."
-    print*, "seed1:", generators(1)%oseed
-    do i = 1,10
-        print*, generators(1)%rnd()
-    enddo
+    write (*, *) ""
+    write (*, *) ""
 
-    write(*,*) ""
-    print*, "seed2:", generators(2)%oseed
-    do i = 1,10
-        print*, generators(2)%rnd()
-    enddo
+    allocate(rndgen_xoshiro256_t :: generators(2))
+    call run_two_streams(generators, "Xoshiro256**", seed1, seed2)
+    deallocate(generators)
 
-    write(*,*) ""
-    write(*,*) "Reseting each generator and using at the same time. Will give the same sequences:"
-    call generators(1)%reset()
-    call generators(2)%reset()
-    do i = 1,10
-        print*, generators(1)%rnd(), generators(2)%rnd()
-    enddo
+contains
 
-end program
+    !> Subrotina genérica que aceita um array de geradores polimórficos
+    subroutine run_two_streams(gens, gen_name, s1, s2)
+        class(rndgen_base_t), intent(inout) :: gens(:)
+        character(len=*), intent(in) :: gen_name
+        integer(kind=i4), intent(in) :: s1, s2
+        integer(kind=i4) :: i
+
+        write(*,*) "=================================================="
+        write(*,*) "  TESTING TWO STREAMS: ", gen_name
+        write(*,*) "=================================================="
+
+        call gens(1)%init(s1)
+        call gens(2)%init(s2)
+
+        write(*,*) "Writing 10 numbers..."
+        ! Usamos 's1' diretamente pois oseed é privado/específico da classe filha
+        print*, "seed1:", s1
+        do i = 1,10
+            print*, gens(1)%rnd()
+        enddo
+
+        write(*,*) ""
+        print*, "seed2:", s2
+        do i = 1,10
+            print*, gens(2)%rnd()
+        enddo
+
+        write(*,*) ""
+        write(*,*) "Reseting each generator and using at the same time. Will give the same sequences:"
+        call gens(1)%reset()
+        call gens(2)%reset()
+        do i = 1,10
+            print*, gens(1)%rnd(), gens(2)%rnd()
+        enddo
+    end subroutine run_two_streams
+
+end program example_multiple_streams
